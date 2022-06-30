8d3c16a8fff08c5389fdd7d505277eef42bf1d50
AndreiVelmezev <velmezev@gmail.com>
2022-06-30 23:22:27 +0300

    EmployeeBook

---
+++ src/Employee.java
@@ -0,0 +1,49 @@
+public class Employee {
+    private String staff;
+    private int salaryStaff;
+    private int departmentNumber;
+    private int id;
+    private static int counter;
+
+    public Employee(String staff, int salaryStaff, int departmentNumber) {
+        this.staff = staff;
+        this.salaryStaff = salaryStaff;
+        this.departmentNumber = departmentNumber;
+        this.id = getCounter();
+
+    }
+
+    public static int getCounter() {
+        return counter++;
+    }
+
+    public String getStaff() {
+        return staff;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSalaryStaff() {
+        return salaryStaff;
+    }
+
+    public int getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public void setStaff(String staff){ this.staff = staff;}
+    public void setSalaryStaff(int salaryStaff){ this.salaryStaff = salaryStaff;}
+    public void setDepartmentNumber(int departmentNumber ){ this.departmentNumber = departmentNumber;}
+
+    @Override
+    public String toString(){
+        return " Employee: "+ staff +
+                " Salary staff: "+ salaryStaff +
+                " Department number: "+ departmentNumber+
+                " Identification number "+ id;
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,85 @@
 public class Main {
+
+    public static void countMinSalary(Employee[] employees) {
+        if (employees.length > 0) {
+            Employee emp = employees[0];
+            for (Employee employee : employees) {
+                if (emp.getSalaryStaff() > employee.getSalaryStaff())
+                    emp = employee;
+            }
+            System.out.println("Minimum wage employee: " + emp.getSalaryStaff() + emp.getStaff() + emp.getId());
+        }
+        else {
+            System.out.println("The array is empty");
+        }
+    }
+
+    public static void countMaxSalary(Employee[] employees) {
+        if (employees.length > 0) {
+            Employee emp = employees[0];
+            for (Employee employee : employees) {
+                if (emp.getSalaryStaff() < employee.getSalaryStaff())
+                    emp = employee;
+            }
+            System.out.println("Employee with the maximum salary: " + emp.getSalaryStaff() + emp.getStaff() + emp.getId());
+        }
+        else {
+            System.out.println("The array is empty");
+        }
+
+    }
+
+    public static int countStaffSalary(Employee[]employees){
+        int sum = 0;
+        for (Employee employee:employees){
+            sum+=employee.getSalaryStaff();
+        }
+        System.out.println("The amount of monthly salary costs for all employees: " + sum);
+        return sum;
+    }
+
+    public static void tupeAllStaff(Employee[] employees) {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+    public static void countAllStaff(Employee[]employees){
+        for (Employee employee: employees){
+            System.out.println("Company employees: "+employee.getStaff());
+        }
+    }
+
+    public static void averageSalary (Employee[] employees){
+        int sum = countStaffSalary(employees);
+        int average = sum / employees.length;
+        System.out.println("Average salary " + average);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Course assignment. Employee Book ");
+
+        Employee[] employee = new Employee[10];
+        employee[0] = new Employee("Ivanov Ivan Ivanovich ", 55_000,  1);
+        employee[1] = new Employee("Petrov Petr Petrovich ", 65_000,  2);
+        employee[2] = new Employee("Sidorov Michail Michailovich ", 99_800,  3);
+        employee[3] = new Employee("Smirnov Semen Evgenievich ", 98_000,  4);
+        employee[4] = new Employee("Rogov Denis Denisovich ", 120_000,  5);
+        employee[5] = new Employee("Lee Denis ", 138_000, 1);
+        employee[6] = new Employee("An Vadim ", 200_000, 2);
+        employee[7] = new Employee("Fedorov Roman Igorevich ", 330_000, 3);
+        employee[8] = new Employee("Simonov Vadim Vadimovich ", 78_000, 4);
+        employee[9] = new Employee("Totov Roman Denisovich ", 140_000, 5);
+
+        tupeAllStaff(employee);
+        countMinSalary(employee);
+        countMaxSalary(employee);
+        countStaffSalary(employee);
+        averageSalary(employee);
+        countAllStaff(employee);
+
+
+
+
     }
 }
